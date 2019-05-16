92693f67be838b40eb1788d0b7c43863f021ec40
Ruben <roobon3d@gmail.com>
2019-05-16 13:49:26 +0200

    Version con play

---
+++ app/src/main/java/basico/android/cftic/edu/milinear/BotonesActivity.java
@@ -0,0 +1,13 @@
+package basico.android.cftic.edu.milinear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BotonesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botones);
+    }
+}
